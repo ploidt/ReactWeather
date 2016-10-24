ef1a53a0b7ad18c3bd91d0f4fa012dda08d14326
ploidt <ploid@Ploid-Macbook.local>
2016-10-24 21:38:04 +0700

    Style Weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) => {
   return (
-    <h4>It is {temp} in {location}</h4>
+    <h3 className="text-center">It is {temp} in {location}</h3>
   )
 };
 
