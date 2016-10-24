fedab316ddf6ece70bcb36d2606edf6ecd329799
ploidt <ploid@Ploid-Macbook.local>
2016-10-24 21:26:01 +0700

    Style all children page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return(
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to Example page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example location to try out</p>
+        <ol>
+          <li>
+            <Link to='/?location=Bangkok'>Bangkok</Link>
+          </li>
+          <li>
+            <Link to='/?location=Phuket'>Phuket</Link>
+          </li>
+        </ol>
       </div>
     );
 };
