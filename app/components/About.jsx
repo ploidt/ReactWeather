5e3f0e1362cea1fda973b8882e18391ff08d0129
ploidt <ploid@Ploid-Macbook.local>
2016-10-24 21:31:58 +0700

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>This is the About page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is the weather application build on React.</p>
+      <p>
+        Here are some of the tool I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JS framework used.
+        </li>
+        <li>
+          <a href="http://weathermap.org">Open Weather Map</a> - I used Open Weather Map tosearch for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 }
